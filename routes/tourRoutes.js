5a8118bda6f7a10616aba40ac3671031219bac86
Gouse Basha <mohammadgous526@gmail.com>
2022-10-06 12:04:37 +0530

    creating documents in mongoose

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,17 +2,12 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
-router.param('id', tourController.checkID)
-
-// Create a checkbody middleware
-// Check if body contains price and name property
-// if not, send back 400 (bad request)
-// Add it to the post handler stack
+// router.param('id', tourController.checkID)
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
--- server.js
+++ server.js
@@ -19,38 +19,7 @@ mongoose
     console.log('DB Connection Succesful');
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A Tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A Tour must have a price']
-  }
-});
 
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  rating: 4.8,
-  price: 997
-});
-
-testTour
-  .save()
-  .then(doc => {
-    console.log(doc);
-  })
-  .catch(err => {
-    console.log('Error : ', err);
-  });
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
