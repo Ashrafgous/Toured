678513b6a4dbbe2c608441cbcd7d567cfcd652d9
Gouse Basha <mohammadgous526@gmail.com>
2022-11-03 13:00:28 +0530

    Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,7 +8,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
-    slug:String,
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A Tour must have a duration']
@@ -54,8 +54,13 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
+
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -71,7 +76,7 @@ tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
- 
+
 // tourSchema.pre('save', function(next){
 //   console.log('Will save document...')
 //   next();
@@ -81,6 +86,20 @@ tourSchema.pre('save', function(next) {
 //   console.log(doc);
 //   next();
 // })
+
+// Query Middleware --------
+// tourSchema.pre('find', function(next) {
+tourSchema.pre('/^find/', function(next) {
+  this.find({secretTour: {$ne:true}})
+  this.start = Date.now()
+  next();
+});
+tourSchema.post(/^find/, function(docs, next){
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  console.log(docs)
+  next();
+})
+
 
 const Tour = mongoose.model('Tour', tourSchema);
 
